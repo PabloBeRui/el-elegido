cf8aeb26c51d877b88705fd0c984c83c24550393
PabloBeRui <pablo.bellon.ruibal@gmail.com>
2024-09-02 13:41:51 +0200

    Enhance Card component with hover effects and state management

--- src/components/helpers/Card.jsx
+++ src/components/helpers/Card.jsx
@@ -1,20 +1,29 @@
 import { Box, Typography } from "@mui/material";
+import { useState } from "react";
 
 // Componente reutilizable para crear cards
 export const Card = ({ title, image, description }) => {
+  // Creo el UseState isHovered para manejar el estado de hover en la imagen
+  const [isHovered, setIsHovered] = useState(false);
+  //Funcion para cambiar el estado
+  const changeHoverState = () => {
+    isHovered ? setIsHovered(false) : setIsHovered(true);
+  };
+
   return (
     <Box
       sx={{
         display: "flex",
-        flexDirection: { xs: "column", sm: "row" }, // Columna en pantallas pequeñas, fila en pantallas medianas y grandes (sin punto de interrupcion desde sm)
+        flexDirection: { xs: "column", sm: "row" }, // Columna en pantallas pequeñas, fila en pantallas medianas y grandes
         alignItems: "center",
         padding: 2,
         border: "1px solid #ddd",
         borderRadius: 2,
         boxShadow: 1,
         margin: 2,
-        maxWidth: 700,
+        maxWidth: 600,
         width: "90%",
+        overflow: "hidden",
       }}>
       <Box
         component="img"
@@ -26,7 +35,13 @@ export const Card = ({ title, image, description }) => {
           borderRadius: 1,
           marginBottom: { xs: 2, sm: 0 },
           marginRight: { sm: 2 },
+          transition: "transform 1.5s ease",
+          "&:hover": {
+            transform: "scale(1.1)",
+          },
         }}
+        onMouseEnter={changeHoverState}
+        onMouseLeave={changeHoverState}
       />
       <Box
         sx={{
@@ -34,12 +49,19 @@ export const Card = ({ title, image, description }) => {
           flexDirection: "column",
           alignItems: { xs: "center", sm: "flex-start" },
           textAlign: { xs: "center", sm: "left" },
+          transition: "filter 0.6s ease",
         }}>
         {/* gutterBottom para dejar un ligero espaciado y body 2 para cuerpo de descripcion ligeramente más pequeño, textScondary color más suave*/}
         <Typography variant="h6" gutterBottom>
           {title}
         </Typography>
-        <Typography variant="body2" color="textSecondary">
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          sx={{
+            filter: isHovered ? "blur(3px)" : "none",
+            transition: "filter 3s ease",
+          }}>
           {description}
         </Typography>
       </Box>
