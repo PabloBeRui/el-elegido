af4fecef3320c9e3698dff5a1f831d00b81904b2
PabloBeRui <pablo.bellon.ruibal@gmail.com>
2024-09-04 17:56:50 +0200

    starting fetch helper

---
+++ src/components/helpers/fetch.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+
+
+ 
+    // UseState de datos, estado de carga y estado de errores
+    
+//     const [data, setdata] = useState(null)
+//     const [loading, setloading] = useState(true)
+//     const [error, seterror] = useState(null)
+
+//     useEffect(() => {
+      
+        
+
+//         //cambio el estado a cargando
+        
+
+    
+     
+    
+
+// };
